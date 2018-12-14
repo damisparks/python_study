2855e7e6c4d2c2b01a1382f522fd7343df9a6a10
Damilola Omifare <omifaredammy@gmail.com>
2018-12-14 00:11:30 +0000

    Docs : Sum the list and average it with length
    Message :
    -> sum the list and average it with length.

--- finding_the_percentage.py
+++ finding_the_percentage.py
@@ -16,14 +16,22 @@ Print one line: The average of the marks obtained by the particular student corr
 """
 # Here is the started code. 
 
-if __name__ == '__main__':
-    n = int(raw_input())
-    student_marks = {}
-    for _ in range(n):
-        line = raw_input().split()
-        name, scores = line[0], line[1:]
-        scores = map(float, scores)
-        student_marks[name] = scores
-    query_name = raw_input()
+n = int(input('Input a number ? '))
+student_marks = {}
+for _ in range(n):
+    name, *line = input('Write a name').split()
+    scores = list(map(float, line))
+    print (line)
+    print (name)
+    #average = scores / len(scores)
+    print(len(scores))
+    student_marks[name] = scores
+    print (student_marks[name])
+    print ('This is your score', scores)
+    print (student_marks)
+query_name = input('What name do you want to query ?: ')
+if query_name in student_marks:
+    print (sum(student_marks[query_name])/len(student_marks[query_name]))
+
 
 #--To be continued later today.--#
