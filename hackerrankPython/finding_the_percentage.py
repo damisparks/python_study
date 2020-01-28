f3a10e253e4769ec9170aed56f7cca6d30baaf4b
Damilola <omifaredammy@gmail.com>
2020-01-28 00:13:08 +0000

    Restructure the project folder.

--- hackerrankPython/finding_the_percentage.py
+++ hackerrankPython/finding_the_percentage.py
@@ -1,29 +1,30 @@
-"""
-- Problem 
-You have a record of  students. Each record contains the student's name, and their percent marks in Maths, Physics and Chemistry. 
-The marks can be floating values. The user enters some integer  followed by the names and marks for students. 
-You are required to save the record in a dictionary data type. 
-The user then enters a student's name. Output the average percentage marks obtained by that student, correct to two decimal places.
-
-- Input Format
-The first line contains the integer , the number of students. The next  lines contains the name and marks obtained by that student separated by a space. The final line contains the name of a particular student previously listed.
-
-- Constraints
-_to be added._
-- Output Format
-Print one line: The average of the marks obtained by the particular student correct to 2 decimal places.
-
-"""
-# Here is the started code. 
-
-if __name__ == '__main__':
-    n = int(raw_input())
-    student_marks = {}
-    for _ in range(n):
-        line = raw_input().split()
-        name, scores = line[0], line[1:]
-        scores = map(float, scores)
-        student_marks[name] = scores
-    query_name = raw_input()
-
-#--To be continued later today.--#
+# hackerrank find_the_percentage
+print('- Problem.\n \
+You have a record of  students. Each record contains the student\'s name, and their percent marks in Maths, Physics and Chemistry.\n \
+The marks can be floating values. The user enters some integer  followed by the names and marks for students. \n \
+You are required to save the record in a dictionary data type. \n \
+The user then enters a student\'s name. Output the average percentage marks obtained by that student, correct to two decimal places.\n\n \
+- Input Format\n\
+-> The first line contains the integer , the number of students. \n\
+-> The next  lines contains the name and marks obtained by that student separated by a space. \n\
+-> The final line contains the name of a particular student previously listed.\n\
+ 2 <= N <= 10\n\
+ 0 <= Marks <= 100 \n\n\
+ - Output Format \n\
+ Print one line: \n\
+ -> The average of the marks obtained by the particular student correct to 2 decimal places') 
+n = int(input('Input a number ? '))
+student_marks = {}
+for _ in range(n):
+    name, *line = input('Write a name').split()
+    scores = list(map(float, line))
+    # print (line)
+    # print (name)
+    # print(len(scores))
+    student_marks[name] = scores
+    # print (student_marks[name])
+    # print ('This is your score', scores)
+    # print (student_marks)
+query_name = input('What name do you want to query ?: ')
+if query_name in student_marks:
+    print ('{0:.2f}'.format(sum(student_marks[query_name])/len(student_marks[query_name])))
--- studentManager/student.py
+++
@@ -1,6 +0,0 @@
-# class contains student name,id, and we can add more. 
-class AddStudent:
-    def __init__(self, name, studentID=322):
-        self.name = name
-        self.studentID = studentID
-        students.append(self)
