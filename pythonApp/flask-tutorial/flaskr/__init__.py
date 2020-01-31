c0e0f39e615cb1cf4e40fb9df695a22cb022ed93
Damilola <omifaredammy@gmail.com>
2020-01-31 01:29:34 +0000

    feat  & test: Add blog blueprint implementation
    - Add blog.py file
    - Define the blueprint and register it in the application factory
    - Create a blog folder in the templates folder.
    - Add index.html to the blog folder for the index view.
    - Add create.html for users to make new posts.
    - Add the update html for updating post.
    - Add delete  functionality to the application.
    -

--- pythonApp/flask-tutorial/flaskr/__init__.py
+++ pythonApp/flask-tutorial/flaskr/__init__.py
@@ -37,4 +37,9 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
+    # Register blog blueprint 
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
