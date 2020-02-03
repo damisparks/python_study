7768bfc92f5a519757137c59d587297d4bda49fb
Damilola <omifaredammy@gmail.com>
2020-02-03 15:48:51 +0000

    feat : Add argparse to create a useful CLI
    - Import argparse
    - Add some test scripts.

---
+++ practice_python/python_in_system/args.py
@@ -0,0 +1,15 @@
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = 'Say a Greeting')
+    parser.add_argument('name', type=str)
+    parser.add_argument('--city', type=str,
+                        default="Lisbon", help="Where are you from ?")
+    
+    args = parser.parse_args()
+    name = args.name
+    city = args.city
+    
+    print(f'Hello! ,{name} from {city}')
+    
+
